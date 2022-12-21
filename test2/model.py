b791c8eefa163e210918ae1c8ae8ffdfcb7f6146
jaehyun-ko <contact@jae-hyun.net>
2022-12-21 21:59:07 +0900

    agent 시작

--- test2/model.py
+++ test2/model.py
@@ -51,5 +51,66 @@ class ReplayBuffer(object):
         return state, action, reward, next_state
     
     
+class Network(nn.Module):
+    def __init__(self, num_actions):
+        super(DQN, self).__init__()
+        self.convlayer = nn.Sequential(nn.Conv2d(4, 32, 8, stride=4),
+                                       nn.batchnorm2d(32),
+                                       nn.Conv2d(32, 64, 4, stride=2),
+                                       nn.batchnorm2d(64),
+                                       nn.Conv2d(64, 64, 3, stride=1),
+                                       nn.batchnorm2d(64))
+        self.fc = nn.Sequential(nn.Linear(3136, 512),
+                                nn.leaky_relu(),
+                                nn.Linear(512, num_actions),
+                                nn.leaky_relu())
 
         
+    def forward(self, x):
+        x = self.convlayer(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+    
+    
+    
+class Agent(nn.Module):
+    def __init__(self, num_actions, device = 'cuda:1' if torch.cuda.is_available() else 'cpu:0')
+        super(Agent, self).__init__()
+        self.num_actions = num_actions
+        self.buffer = ReplayBuffer(replay_buffer_size, batch_size)
+        self.q_network = Network(num_actions).to(device)
+        self.target_network = Network(num_actions).to(device)
+        self.target_network.load_state_dict(self.q_network.state_dict())
+        self.optimizer = optim.Adam(self.q_network.parameters(), lr=learning_rate)
+        
+    def update_target_network(self):
+        self.target_network.load_state_dict(self.q_network.state_dict())
+        
+    def get_action(self, state, epsilon):
+        if np.random.rand() <= epsilon:
+            return random.randrange(self.num_actions)
+        else:
+            state = torch.FloatTensor(state).to(device)
+            return self.q_network(state).argmax().item()
+        
+    def train(self):
+        if len(self.buffer) < batch_size:
+            return 0.0
+        
+        state, action, reward, next_state = self.buffer.sampling()
+        
+        q_value = self.q_network(state).gather(1, action.unsqueeze(1)).squeeze(1)
+        next_q_value = self.target_network(next_state).max(1)[0]
+        expected_q_value = reward + gamma*next_q_value
+        
+        loss = F.smooth_l1_loss(q_value, expected_q_value.detach())
+        
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
+        return loss.item()
+    
+    def save_model(self, path):
+        torch.save(self.q_network.state_dict(), path)
